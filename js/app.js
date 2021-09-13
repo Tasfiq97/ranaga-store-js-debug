9bee15122b001b73c516706774665c01b17d3ed4
Tasfiq97 <tasfiqulazad200@gmail.com>
2021-09-13 15:35:42 +0600

    new commit

--- js/app.js
+++ js/app.js
@@ -15,13 +15,16 @@ const showProducts = (products) => {
     div.classList.add("product");
     div.innerHTML = `<div class="single-product">
       <div>
-    <img class="product-image img-fluid" src=${image}></img>
+    <img class="product-image" src=${image}></img>
       </div>
-      <h3>${product.title}</h3>
+      <h3 class="mt-3">${product.title}</h3>
       <p>Category: ${product.category}</p>
-      <h2>Price: $ ${product.price}</h2>
-      <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
-      <button id="details-btn" class="btn btn-danger">Details</button></div>
+      <h5>Price: $ ${product.price}</h5>
+      <div class="card-footer mt-1 d-flex justify-content-around">
+      <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class=" btn-color">add to cart</button>
+      <button id="details-btn" class=" btn btn-dark">Details</button>
+      </div>
+      </div>
       `;
     document.getElementById("all-products").appendChild(div);
   }
@@ -79,3 +82,13 @@ const updateTotal = () => {
     getInputValue("total-tax");
   document.getElementById("total").innerText = grandTotal.toFixed(2);
 };
+
+
+// show total in modal 
+
+const showTotalInModal=()=>{
+ const modalTotal= document.getElementById("total").innerText;
+ document.getElementById("total-cost").innerHTML=`
+ your total cost is :$ ${modalTotal}
+ `;
+}
